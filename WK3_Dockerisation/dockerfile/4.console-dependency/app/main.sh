2df7c9f27d9ef18ffed958f5583eafd812a0a5ae
YuJiangRen <yu.wang.jr@gmail.com>
2022-12-31 10:17:16 +1100

    wk3 docker bugfix for broken metaweather api

--- WK3_Dockerisation/dockerfile/4.console-dependency/app/main.sh
+++ WK3_Dockerisation/dockerfile/4.console-dependency/app/main.sh
@@ -3,7 +3,7 @@
 echo "Press Ctrl+C any time to stop the application."
 echo
 
-for i in $(seq 5); do
+for i in $(seq 2); do
     for key in sy au ing ba lu pa ji san ; do
         echo "Searching all cities that contains '${key}' keyword ..."
         curl http://citymatcher?city=${key} 2>/dev/null
